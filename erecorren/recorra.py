77d1a564ee27e0b3a34bdc825914af471259fdf0
mifegui <mife@mifegui.xyz>
2021-11-21 12:43:13 -0300

    Fazer serviço de recorrencia

---
+++ erecorren/recorra.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, json, request
+app = Flask(__name__)
+
+eentregasUrl = "http://127.0.0.1:5000"
+
+@app.route('/list')
+def list():
+    return json.dumps(getData())
+    
+@app.route('/recorra', methods=['POST'])
+def recorra():
+    data = json.loads(request.data)
+
+    if (data.recurr == 'minute'):
+        recurr = "* * * * *"
+    if (data.recurr == 'hourly'):
+        recurr = "0 * * * *"
+    if (data.recurr == 'monthly'):
+        recurr = "0 0 1 * *"
+
+    command = ('(crontab -l 2>/dev/null; echo "' + recurr +
+                ' curl -x POST -H "Content-Type: application/json" ' + 
+                "-d '"  + json.dumps(data) + "' " + eentregasUrl + "/buy" +
+                '") | crontab -'
+            )
+    os.system(command)
+
+def getData():
+    return os.popen('crontab -l').read()
+
+
+if __name__ == '__main__':
+   app.run(port = 5000, debug = True)
