cf4ec375f0543fe9f5ce61603527dc811481b0e9
mifegui <mife@mifegui.xyz>
2021-11-24 22:53:00 -0300

    Adicionar serviço de compras mais simples

---
+++ ecomprar/ecomprar.py
@@ -0,0 +1,43 @@
+import requests
+from flask import Flask, json, request, render_template, redirect
+
+app = Flask(__name__)
+
+entregasUrl = "34.238.233.0:5000"
+recorrenUrl = "54.237.174.74:5001"
+
+entregasUrl = "http://"+entregasUrl
+recorrenUrl = "http://"+recorrenUrl
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html", eUrl=entregasUrl + "/list", rUrl=recorrenUrl + "/list"
+    )
+
+
+@app.route("/comprar", methods=["POST", "GET"])
+def comprar():
+    if request.method == "GET":
+        return render_template(
+            "index.html", eUrl=entregasUrl + "/list", rUrl=recorrenUrl + "/list"
+        )
+    data = request.form
+
+    print(data)
+    whatSend = {"customerEmail": data["email"], "products": [{"name": data["produto"]}]}
+
+    url = entregasUrl + "/buy"
+    if data["recurrence"]:
+        url = recorrenUrl + "/recorra"
+        whatSend["recurr"] = data["recurrence"]
+    res = requests.post(url, json=whatSend)
+
+    return render_template(
+        "index.html", eUrl=entregasUrl + "/list", rUrl=recorrenUrl + "/list"
+    )
+
+
+if __name__ == "__main__":
+    app.run(port=5001, host="0.0.0.0", debug=True)
--- mainSetup.sh
+++ mainSetup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 #ecommerceUrl="184.73.130.93:80"
+comprasUrl="184.73.130.93:80"
 entregasUrl="34.238.233.0:5000"
 recorrenUrl="54.237.174.74:5001"
 rdsUrl="database-1.cl4ej0uksgat.us-east-1.rds.amazonaws.com"
 
 #ecommerceSsh=ec2-user@${ecommerceUrl}
+comprasSsh=ec2-user@${ecommerceUrl}
 entregasSsh=ec2-user@${entregasUrl}
 recorrenSsh=ec2-user@${recorrenUrl}
 
 #ecommerceSsh=${ecommerceSsh/:*/}
+comprasSsh=${ecommerceSsh/:*/}
 entregasSsh=${entregasSsh/:*/}
 recorrenSsh=${recorrenSsh/:*/}
 
 sshKey="vockey.pem"
+
+sed -i 's|^entregasUrl.*|entregasUrl="'$entregasUrl'"|g' ecomprar/setup.sh
+sed -i 's|^recorrenUrl.*|recorrenUrl="'$recorrenUrl'"|g' ecomprar/setup.sh
 
 sed -i 's|^entregasUrl.*|entregasUrl="'$entregasUrl'"|g' ecommerce/setup.sh
 sed -i 's|^recorrenUrl.*|recorrenUrl="'$recorrenUrl'"|g' ecommerce/setup.sh
@@ -23,12 +29,14 @@ sed -i 's|^entregasUrl.*|entregasUrl="'$entregasUrl'"|g' erecorren/setup.sh
 sed -i 's|^rdsUrl.*|rdsUrl="'$rdsUrl'"|g' eentregas/setup.sh
 
 #scp -i $sshKey ecommerce/setup.sh ${ecommerceSsh}:
+scp -i $sshKey ecomprar/setup.sh ${comprasSsh}:
 scp -i $sshKey eentregas/setup.sh ${entregasSsh}:
 scp -i $sshKey erecorren/setup.sh ${recorrenSsh}:
 
 #ssh -i $sshKey $ecommerceSsh 'bash setup.sh' &
+ssh -i $sshKey $comprasSsh 'bash setup.sh' &
 ssh -i $sshKey $entregasSsh 'bash setup.sh' &
 ssh -i $sshKey $recorrenSsh 'bash setup.sh' &
 
-cd ecommerce/sereiada
-bash ../setup.sh
+#cd ecommerce/sereiada
+#bash ../setup.sh
